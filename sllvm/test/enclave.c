5a1cef6d0fdd74193a60efe8d8a6028b0bf40fc7
Hans Winderix <hans.winderix@cs.kuleuven.be>
2018-11-13 12:56:21 +0100

    Add minimal Sancus compilation target

---
+++ sllvm/test/enclave.c
@@ -0,0 +1,8 @@
+#include "sm_support.h"
+
+DECLARE_SM(enclave, 42);
+
+int SM_ENTRY(enclave) enclave_public_get_secret(void)
+{
+  return 10;
+}
---
+++ sllvm/test/test.c
@@ -0,0 +1,10 @@
+#include "sm_support.h"
+
+int SM_ENTRY(enclave) enclave_public_get_secret(void);
+
+__attribute__((section(".init9"),aligned(2)))
+int main(void)
+{
+  /* Call to enclave code from unprotected code */
+  return enclave_public_get_secret();
+}
