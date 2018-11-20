fded413aa9d20b1318d23786b8adee3c1a5053e4
Hans Winderix <hans.winderix@cs.kuleuven.be>
2018-11-20 16:32:43 +0100

    Strip file extensions and replace '-' for '_' when deriving module name from file name

--- sllvm/lib/Transforms/SLLVM/Transformation.cpp
+++ sllvm/lib/Transforms/SLLVM/Transformation.cpp
@@ -10,10 +10,22 @@ bool SLLVMTransformation::runOnModule(Module &M) {
   bool result = false;
 
   if (getAnalysis<SLLVMAnalysis>().getResults().isPM()) {
+    auto FN = M.getSourceFileName();
+
+    // Strip file extensions
+    auto idx = FN.find_first_of(".");
+    if (idx != std::string::npos) {
+      FN = FN.substr(0, idx);
+    }
+
+    std::replace(FN.begin(), FN.end(), '-', '_');
+
+    auto N = llvm::MDString::get(M.getContext(), FN);
+
     // TODO: Come up with a better module name (see todo list)
-    auto N = llvm::MDString::get(M.getContext(), M.getSourceFileName());
     // TODO: What happens when llvm-linking (which name is taken?)
     M.addModuleFlag(Module::Override, sllvm::module_flag_pm, N);
+
     result = true;
   }
 
