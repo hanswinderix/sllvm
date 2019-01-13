de763bf223f483c2192f897a0c74c581bb5975b2
Hans Winderix <hans.winderix@skynet.be>
2019-01-13 14:53:59 +0100

    Add private data

--- sancus/test/hello/hello.c
+++ sancus/test/hello/hello.c
@@ -1,9 +1,13 @@
 #include "hello.h"
 #include "sancus_support/sm_io.h"
 
+static int secret = 42;
+
 static void print_hello(void)
 {
-  pr_info1("Hello from %d\n", sancus_get_self_id());
+  pr_info2("Hello from SM %d. My secret is %d \n",
+      sancus_get_self_id(),
+      secret);
 }
 
 void hello_enter(void)
