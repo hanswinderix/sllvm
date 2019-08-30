a17723b7b675dc71b380fa91b95e97f08bf270a3
Hans Winderix <hans.winderix@skynet.be>
2019-08-30 22:29:44 +0200

    [Nemdef] Continued work on postprocessing the vcd files

--- test/sancus/pp.py
+++ test/sancus/pp.py
@@ -4,21 +4,23 @@ import re
 import matplotlib.pyplot as plt
 
 header_pos = 0
-header = [
-'0 time',
-'1 tb_openMSP430.cur_tsc[63:0]',
-'2 tb_openMSP430.msp_debug_0.inst_pc[15:0]',
-'3 tb_openMSP430.msp_debug_0.inst_full[255:0]',
-'',
-#'0                1    2                                                                3',
-'========================================================================================',
-'0                x    x                                                                x',
-]
+header = """
+0 time
+1 tb_openMSP430.cur_tsc[63:0]
+2 tb_openMSP430.msp_debug_0.inst_pc[15:0]
+3 tb_openMSP430.dut.execution_unit_0.exec_sm
+4 tb_openMSP430.msp_debug_0.inst_full[255:0]
+
+==========================================================================================
+0                x    x 0                                                                x
+""".strip().split('\n')
 
 # Parse command line
 assert len(sys.argv) > 1, "Argument expected"
 exename       = sys.argv[1]
 vcdcat_output = "%s.vcdcat" % exename
+
+data = []
 
 # Post-process vcdcat output
 with open(vcdcat_output) as f:
@@ -26,20 +28,34 @@ with open(vcdcat_output) as f:
   for line in f:
     try:
       # Parse the next line of the vcdcat output
-      t, cur_tsc, inst_pc, inst_full = line.split()
+      t, cur_tsc, inst_pc, exec_sm, inst_full = line.split()
       inst_pc = int(inst_pc, 16)
       if (prev_inst_pc != inst_pc):
         cur_tsc   = int(cur_tsc, 16)
+        exec_sm   = int(exec_sm, 16)
         inst_full = inst_full[:-1] # Drop trailing 'L'
         inst_full = [inst_full[i:i + 2] for i in range(0, len(inst_full), 2)]
         inst_full = "".join([chr(int(x, 16)) for x in inst_full])
-        print('%d %X "%s"' % (cur_tsc, inst_pc, inst_full))
+        inst_full = inst_full.replace('\0', '').strip()
+        data.append([cur_tsc, inst_pc, exec_sm, inst_full])
         prev_inst_pc = inst_pc
     except:
       # Assert this is the expected line of the header and just skip it
       # TODO: This should also include header line number 5
-      assert line.strip() == header[header_pos], (line, header[header_pos])
+      assert line.strip() == header[header_pos].strip(), \
+                                               (line, header[header_pos])
       header_pos = header_pos + 1
+
+# Compute instruction latencies
+z = list(zip(data[1:], data[:-1]))
+for idx in range(len(data)-1):
+  l, r = z[idx]
+  data[idx][0] = l[0] - r[0]
+
+#############################################################################
+
+for record in data:
+  print(record)
 
 """
 with open(fname) as f:
