9060d709beba2a0952615f223b5026a3706ff3f5
Hans Winderix <hans.winderix@skynet.be>
2019-08-30 22:28:07 +0200

    Enable/disable Sancus-Step via preprocessor

--- test/sancus/nemdef.h
+++ test/sancus/nemdef.h
@@ -4,8 +4,14 @@
 #define _eentry __attribute__((eentry))
 #define _secret __attribute__((secret))
 
+#ifdef SANCUS_STEP_ENABLED 
 #define SS_MOUNT(_f_, ...)               \
   __ss_mount(#_f_ "(" #__VA_ARGS__ ")"); \
   _f_(__VA_ARGS__);
+#else
+#define SS_MOUNT(_f_, ...)                        \
+  printf("ss_mount: " #_f_ "(" #__VA_ARGS__ ")\n"); \
+  _f_(__VA_ARGS__);
+#endif
 
 #endif
