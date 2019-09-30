2c5c96d4a3cd4396b79c5af206c173a8a3a93e1c
Hans Winderix <hans.winderix@cs.kuleuven.be>
2019-09-30 11:17:02 +0200

    Add ELF methods: find_section_by_address, find_symbol_by_address, extract_bytes

--- sancus/crypto/elf.py
+++ sancus/crypto/elf.py
@@ -6,16 +6,34 @@ class ELF:
 
   def __init__(self, fname):
     self.name = fname
-    self.elf = ELFFile(open(fname, 'rb')) 
+    self.elf = ELFFile(open(fname, 'rb'))
 
   def get_name(self):
     return self.name
+
+  def find_section_by_address(self, addr):
+    for section in self.elf.iter_sections():
+      sh_addr = section['sh_addr']
+      sh_size = section['sh_size']
+      if (addr >= sh_addr) and (addr < (sh_addr + sh_size)):
+        return section
+    return None
 
   def find_symbol_by_name(self, name):
     for section in self.elf.iter_sections():
       if section.header['sh_type'] == 'SHT_SYMTAB':
         for sym in section.iter_symbols():
           if sym.name == name:
+            return sym
+    return None
+
+  def find_symbol_by_address(self, addr):
+    for section in self.elf.iter_sections():
+      if section.header['sh_type'] == 'SHT_SYMTAB':
+        for sym in section.iter_symbols():
+          # TODO: Relax condition
+          #         (addr should be within range [st_value, stvalue+size[)
+          if sym['st_value'] == addr:
             return sym
     return None
 
@@ -28,6 +46,18 @@ class ELF:
           if m:
             matches.append((m, sym))
     return matches
+
+  def extract_bytes(self, symbol):
+    section = self.find_section_by_address(symbol['st_value'])
+    assert section
+    offset = symbol['st_value'] - section['sh_addr']
+    #offset_in_elf = section['sh_offset'] + offset
+    # TODO: Check that result is within bounds
+    return section.data()[offset:offset+symbol['st_size']]
+
+  ###########################################################################
+  # Text section specific function
+  # TODO: Replace them with more generic ones
 
   def get_text_section(self):
     section = self.elf.get_section_by_name(".text")
