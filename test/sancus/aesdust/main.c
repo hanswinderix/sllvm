d82b4f525675f7df64a0a1e9cf5bd9389bae6fed
Hans Winderix <hans.winderix@cs.kuleuven.be>
2021-02-16 18:45:19 +0100

    Add aes-dust benchmark (does not compile)

---
+++ test/sancus/aesdust/main.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include "sancus_support/sm_io.h"
+#include "sancus_support/sancus_step.h"
+
+#include "nemdef.h"
+#include "aesdust.h"
+
+asm(".section __interrupt_vector_10,\"ax\",@progbits \n\t"
+    ".word timerA_isr_entry2                         \n\t");
+
+void attack(void)
+{
+  __ss_print_latency();
+}
+
+int main(void)
+{
+  msp430_io_init();
+
+#ifdef SANCUS_STEP_ENABLED
+  __ss_init();
+#endif
+
+  sancus_enable(&aesdust);
+
+  {
+    uint8_t iv[AES_BLK_LEN] = {
+      0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
+      0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f
+    };
+    uint8_t mk[AES_KEY_LEN] = {
+      0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
+      0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f
+    };
+    uint8_t data[] = {
+      0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
+      0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f
+    };
+
+    ATTACK(aes_ofb, sizeof(data)/sizeof(data[0]), iv, data, mk);
+  }
+
+  EXIT();
+
+  return 0;
+}
+
+SANCUS_STEP_ISR_ENTRY2(attack, __ss_end);
